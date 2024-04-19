48342b43083d0f3c28f18217a39a471e432d2142
Sutanissaya <167442356+sutanissaya@users.noreply.github.com>
2024-04-19 23:27:11 +0700

    Update dict.py

--- dict.py
+++ dict.py
@@ -10,8 +10,8 @@ LOGGER = get_logger(__name__)
 st.set_page_config(page_title="Kamus Pāḷi-Indonesia", page_icon="🪷")
 
 st.markdown("<h1 style='text-align: center;'>🪷 Kamus Pāḷi-Indonesia 🪷</h1>", unsafe_allow_html=True)
-st.markdown("<h4 style='text-align: center;'>berdasarkan <em>Concise Pali-English Dictionary</em> oleh A. P. Buddhadatta (1992)</h3>", unsafe_allow_html=True)
-st.markdown("<h5 style='text-align: center;'>terjemahan oleh SuttaCentral</h3>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center;'>berdasarkan <em>Concise Pali-English Dictionary</em> oleh A. P. Buddhadatta (1992)</h4>", unsafe_allow_html=True)
+st.markdown("<h5 style='text-align: center;'>terjemahan oleh SuttaCentral</h5>", unsafe_allow_html=True)
 st.divider()
 
 st.sidebar.write("Tampilkan kamus di [SuttaCentral](https://suttacentral.net/define/a%E1%B9%81sa?lang=id)")
