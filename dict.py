49b594c93c46863894dd82df1559376fbdda3fbf
Sutanissaya <167442356+sutanissaya@users.noreply.github.com>
2024-04-19 17:10:05 +0700

    Update dict.py

--- dict.py
+++ dict.py
@@ -1,3 +1,15 @@
+import streamlit as st
+from streamlit.logger import get_logger
+from streamlit.hello.utils import show_code
+import altair as alt
+import numpy as np
+import pandas as pd
+
+LOGGER = get_logger(__name__)
+
+st.set_page_config(page_title="Dictionary Test", page_icon="🌴")
+
+
 thisdict =	{
   "brand": "Ford",
   "model": "Mustang",
