94520e5314c0adff7c151419c5f1eeef37b3fb29
Sutanissaya <167442356+sutanissaya@users.noreply.github.com>
2024-04-19 17:54:13 +0700

    Update dict.py

--- dict.py
+++ dict.py
@@ -17,7 +17,7 @@ thisdict = {
     "definition": "m.  bahu (<i>aṁse karoti</i>  meletakkan di bahu, menyandang, memanggul, memikul); bagian, hal berbagi; sudut, penjuru, ujung, tepi; <b>~kūṭa</b> m. nt. pundak, bahu."
 }
 
-x = thisdict
+x = thisdict.keys():
 
 for x in thisdict:
   st.write(x)
