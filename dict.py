57416c59686dd579fd64624ded33d33de549291b
Sutanissaya <167442356+sutanissaya@users.noreply.github.com>
2024-04-19 17:31:42 +0700

    Update dict.py

--- dict.py
+++ dict.py
@@ -13,6 +13,9 @@ st.set_page_config(page_title="Dictionary Test", page_icon="🌴")
 thisdict = {
     "entry": "amsa",
     "definition": "m.  bahu (<i>aṁse karoti</i>  meletakkan di bahu, menyandang, memanggul, memikul); bagian, hal berbagi; sudut, penjuru, ujung, tepi; <b>~kūṭa</b> m. nt. pundak, bahu."
+},  {
+    "entry": "akal",
+    "definition": "a.  sakit, tak sehat."
 }
 
 
