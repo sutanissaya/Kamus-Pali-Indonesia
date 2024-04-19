6103c750facca947cf01cc6cf00325f7de63e651
Sutanissaya <167442356+sutanissaya@users.noreply.github.com>
2024-04-19 23:51:47 +0700

    Update pli-id.py

--- pli-id.py
+++ pli-id.py
@@ -16,15 +16,12 @@ st.divider()
 
 st.sidebar.write("Tampilkan kamus di [SuttaCentral](https://suttacentral.net/define/a%E1%B9%81sa?lang=id)")
 st.sidebar.write("Unduh [Kamus Pāḷi-Indonesia (JSON)](https://github.com/suttacentral/sc-data/blob/main/dictionaries/simple/id/pli2id_cped.json)")
+st.sidebar.write("Lihat [Lisensi](https://github.com/sutanissaya/Kamus-Pali-Indonesia/blob/main/LICENSE)")
 
 st.sidebar.divider()
 
 st.sidebar.write("Kami berusaha untuk memperbaiki dan mengembangkan situs web ini. Jika Anda memiliki saran atau ide, kami akan menerimanya dengan senang hati melalui [email](mailto:sutanissaya@gmail.com).")
-st.sidebar.write("_Karya di situs web berlisensi di bawah [CC0-1.0 Universal](https://github.com/sutanissaya/palijuncturesplitter/blob/main/LICENSE). Anda dipersilahkan untuk mengembangkan, memodifikasi, memasukkan ke dalam karya lain, menggunakan kembali, dan mendistribusikan sebebas mungkin dalam bentuk apa pun dan untuk tujuan apa pun, termasuk namun tidak terbatas pada tujuan komersial._")
-
-
-
-
+st.sidebar.write("_Karya di situs web berlisensi di bawah [CC0-1.0 Universal](https://creativecommons.org/publicdomain/zero/1.0/). Anda dipersilahkan untuk mengembangkan, memodifikasi, memasukkan ke dalam karya lain, menggunakan kembali, dan mendistribusikan sebebas mungkin dalam bentuk apa pun dan untuk tujuan apa pun, termasuk namun tidak terbatas pada tujuan komersial._")
 
 """
 aṁsa: m.  bahu (_aṁse karoti_  meletakkan di bahu, menyandang, memanggul, memikul); bagian, hal berbagi; sudut, penjuru, ujung, tepi; **~kūṭa**m. nt. pundak, bahu.\n
